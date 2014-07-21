8caa373d83d7e6886172d2f835c1561c62bf5966
Kylie Jue <kjue25@gmail.com>
2014-07-21 16:58:42 -0700

    Validation for panel names works.

--- app/models/page.rb
+++ app/models/page.rb
@@ -10,5 +10,22 @@ class Page < ActiveRecord::Base
   accepts_nested_attributes_for :m_selectpanels, allow_destroy: true
 
   validates :site_name, presence: true
-  #accepts_nested_attributes_for :panels, :reject_if => lambda {|a| a[:panel_name].blank? }, :allow_destroy => true
+  validate :require_panel_names
+
+  private
+    def require_panel_names
+      for panel in text_panels
+        errors.add(:base, "No text panel name.") if panel.panel_name.blank?
+      end
+      for panel in pictures
+        errors.add(:base, "No picture panel name.") if panel.panel_name.blank?
+      end
+      for panel in s_selectpanels
+        errors.add(:base, "No single select panel name.") if panel.panel_name.blank?
+      end
+      for panel in m_selectpanels
+        errors.add(:base, "No multi select panel name.") if panel.panel_name.blank?
+      end
+    end
 end
+
