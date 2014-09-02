3f73da4f06eb14e4f4ed753ea2032ebe59a3f36a
Kylie Jue <kjue25@gmail.com>
2014-09-02 11:03:15 -0700

    Nested form gem works - need to have form save on fieldAdded events.

--- app/assets/javascripts/save.js
+++ app/assets/javascripts/save.js
@@ -1,4 +1,7 @@
-function Save() {
+$(document).on('nested:fieldAdded', function(event) {
+
+  var field = event.field;
+  console.log(field);
   $("#pageform").submit(function(e) {
     var form = $(this);
     var formURL = form.attr("action");
@@ -15,28 +18,28 @@ function Save() {
       cache: false,
       processData: false,
       success: function(data){
-        console.log(data); 
-        var panels = JSON.parse(data);
-        console.log(panels);
-        console.log(toParse);
-        for (i=0; i < toParse.length; i++) {
-          var name = toParse[i].name;
-          var id = /\d{13}/g;
-          var result = name.match(id);
-          console.log(name + ": " + result);
-          if (result != null) {
-            //Find panel in data with largest id and render its view
-            //Delete toParse[i]  
-          }
-          /*
-          var isId = name.search("id");
-          var isTag = name.search("tags_attributes");
-          var isOption = name.search("options_attributes");
-          if (isId != -1 && isTag == -1 && isOption == -1) {
-            console.log(name + ": " + isId);
-          }
-          */
+        /*
+           console.log(data); 
+           var panels = JSON.parse(data);
+           console.log(panels);
+           console.log(toParse);
+           for (i=0; i < toParse.length; i++) {
+           var name = toParse[i].name;
+           var id = /\d{13}/g;
+           var result = name.match(id);
+           console.log(name + ": " + result);
+           if (result != null) {
+        //Find panel in data with largest id and render its view
+        //Delete toParse[i]  
+        }
+        var isId = name.search("id");
+        var isTag = name.search("tags_attributes");
+        var isOption = name.search("options_attributes");
+        if (isId != -1 && isTag == -1 && isOption == -1) {
+        console.log(name + ": " + isId);
+        }
         } 
+         */
         //Refresh panels and preview here
       }
     });
@@ -45,4 +48,4 @@ function Save() {
   });
 
   $("#pageform").submit();
-}
+});
