b4cb134f1921b763bc78be65dd599b0d65389f10
Kylie Jue <kjue25@gmail.com>
2014-09-03 15:06:43 -0700

    Save code cleaned up and comments added. Still buggy if panels added too quickly.

--- app/assets/javascripts/save.js
+++ app/assets/javascripts/save.js
@@ -28,18 +28,9 @@ $(document).on('nested:fieldAdded', function(event) {
 
         //Changes all temporarily generated IDs to actual panel ID
         for (i=0; i<fields.length; i++) {
-          //Storing panel type if not yet defined
           if(fields[i].htmlFor != undefined) {
-            if (panel_type == undefined) {
-              if(fields[i].htmlFor.match('text_panels_attributes')){
-                panel_type = 'text_panels';
-              } else if(fields[i].htmlFor.match('pictures_attributes')){
-                panel_type = 'pictures';
-              } else if(fields[i].htmlFor.match('s_selectpanels_attributes')){
-                panel_type = 's_selectpanels';
-              } else if(fields[i].htmlFor.match('m_selectpanels_attributes')){
-                panel_type = 'm_selectpanels';
-              }
+            if (panel_type == undefined) { //Storing panel type if not yet defined
+              panel_type = get_panel_type(fields[i].htmlFor);
             }
             fields[i].htmlFor = fields[i].htmlFor.replace(/\d{13}/g, id);
           }
@@ -48,7 +39,6 @@ $(document).on('nested:fieldAdded', function(event) {
           }
           if (fields[i].name != undefined) {
             fields[i].name = fields[i].name.replace(/\d{13}/g, id);
-
           }
           tags = $(fields[i]).children();
           for (k=0; k<tags.length; k++) {
@@ -56,7 +46,7 @@ $(document).on('nested:fieldAdded', function(event) {
             for (j=0; j<tag_fields.length; j++) {
               relabel(tag_fields[j], id, panel_tags[k].id);
             }
-            $(document.getElementById("tags")).children()[k].innerHTML += '<input id="page_'+panel_type+'_attributes_'+id+'_tags_attributes_'+panel_tags[k].id+'_id" name="page['+panel_type+'_attributes]['+id+'][tags_attributes]['+panel_tags[k].id+'][id]" type="hidden" value="'+panel_tags[k].id+'">';
+            $(document.getElementById("tags")).children()[k].innerHTML += '<input id="page_'+panel_type+'_attributes_'+id+'_tags_attributes_'+panel_tags[k].id+'_id" name="page['+panel_type+'_attributes]['+id+'][tags_attributes]['+panel_tags[k].id+'][id]" type="hidden" value="'+panel_tags[k].id+'">'; //Add hidden field for tag id
           }
         }
         document.getElementById(panel_fields_id).innerHTML += '<input id="page_'+panel_type+'_attributes_'+id+'_id" name="page[' +panel_type+ '_attributes]['+id+'][id]" type="hidden" value="'+id+'">'; //Add hidden field for panel id
@@ -69,6 +59,15 @@ $(document).on('nested:fieldAdded', function(event) {
 
   $("#pageform").submit(); //Calls above submit function
 });
+
+//Returns the panel type based on form label field
+function get_panel_type(label) {
+  if(label.match('text_panels_attributes')){return 'text_panels';}
+  if(label.match('pictures_attributes')){return 'pictures';}
+  if(label.match('s_selectpanels_attributes')){return 's_selectpanels';}
+  if(label.match('m_selectpanels_attributes')){return 'm_selectpanels';}
+  return undefined;
+}
 
 //Resets ID and name for tag form fields
 function relabel(tag, id, tag_id) {
